c57acb269de673b92ee938532e901acdb07645e4
Ramzi Atrous <atrous.ramzi@gmx.de>
2023-11-22 16:14:37 +0100

    website update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-      <h1>My Techstarter Website über github actions </h1>
+      <h1>My Techstarter Website über github actions .</h1>
   );
 }
 
